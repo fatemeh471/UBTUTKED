52f962bbee328fea1356a4e8b65551bcd4ffc1f8
eisavand_fa <esavand.fa@gmail.com>
2025-01-12 15:22:55 +0330

    first commit

---
+++ amazona/frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <header className="">
+        <a target="_blank">amazona</a>
+      </header>
+      <main>list product</main>
+    </div>
+  );
+}
+
+export default App;
